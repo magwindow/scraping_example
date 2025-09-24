c5cb9e8d200e935dc7c836a41ce14265f4ca20fc
magwindow <antosha.petrakov@yandex.ru>
2025-09-24 16:56:44 +0300

    Added parsing page 1

--- quotes_to_scrape.py
+++ quotes_to_scrape.py
@@ -1,22 +1,31 @@
+from time import sleep
+
 from requests import Session
 from bs4 import BeautifulSoup
 
+# предоставляем информацию о нас, сайту
 headers = {"User-Agent": "CrookedHands/2.0 (EVM x8), CurlyFingers20/1;p"}
 
+# POST - авторизация
 session = Session()
-
 session.get("https://quotes.toscrape.com/", headers=headers)
 response = session.get("https://quotes.toscrape.com/login", headers=headers)
-
 soup = BeautifulSoup(response.text, "lxml")
 
-# Получаем csrf_token
-token = soup.find("form").find("input").get("value")
-
+token = soup.find("form").find("input").get("value")  # Получаем csrf_token
 data = {"csrf_token": token, "username": "anonim", "password": "anonim"}
 
 # Авторизуемся на сайте
-result = session.post("https://quotes.toscrape.com/login", headers=headers, data=data, allow_redirects=True)
+session.post("https://quotes.toscrape.com/login", headers=headers, data=data, allow_redirects=True)
 
-print(result.text)
+# Парсинг страницы
+url = f'https://quotes.toscrape.com/page/1/'
+response = session.get(url, headers=headers)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes_div = soup.find_all('div', class_='quote')
 
+for num, item in enumerate(quotes_div, 1):
+    sleep(1)
+    quote = item.find('span', class_='text').text
+    author = item.find('small', class_='author').text
+    print(f'{num}. {quote}\n{author}')
