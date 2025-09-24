000d014cc6f4350691b4144194e4f5638f04f41f
magwindow <antosha.petrakov@yandex.ru>
2025-09-24 17:04:25 +0300

    Added pagination pages

--- quotes_to_scrape.py
+++ quotes_to_scrape.py
@@ -18,14 +18,24 @@ data = {"csrf_token": token, "username": "anonim", "password": "anonim"}
 # Авторизуемся на сайте
 session.post("https://quotes.toscrape.com/login", headers=headers, data=data, allow_redirects=True)
 
-# Парсинг страницы
-url = f'https://quotes.toscrape.com/page/1/'
-response = session.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'lxml')
-quotes_div = soup.find_all('div', class_='quote')
+# Начальная страница
+page = 1
 
-for num, item in enumerate(quotes_div, 1):
-    sleep(1)
-    quote = item.find('span', class_='text').text
-    author = item.find('small', class_='author').text
-    print(f'{num}. {quote}\n{author}')
+while True:
+    # Парсинг страницы
+    url = f'https://quotes.toscrape.com/page/{page}/'
+    response = session.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes_div = soup.find_all('div', class_='quote')
+
+    if len(quotes_div) == 0:  # Если на странице больше нет цитат
+        print(f'Завершение на странице {page}, так как цитаты закончились.')
+        break
+
+    for num, item in enumerate(quotes_div, 1):
+        sleep(1)
+        quote = item.find('span', class_='text').text
+        author = item.find('small', class_='author').text
+        print(f'{(page-1) * 10 + num}. {quote}\n{author}')
+
+    page += 1  # Переход на следующую страницу
